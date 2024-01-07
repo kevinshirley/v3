e9b4804c31184eb0b8802527990d35e310c158b6
Kevin Shirley <kevinxshirley@gmail.com>
2024-01-07 12:37:58 -0500

    Container spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
+import { LinkedInLogoIcon, GitHubLogoIcon, EnvelopeClosedIcon } from '@radix-ui/react-icons';
+
 export default function Home() {
   return (
-    <div>
-      <div>
-        <header>
+    <div className='bg-slate-900 min-h-screen lg:py-0 lg:px-24'>
+      <div className='flex flex-col md:flex-row w-full'>
+        <header className='w-1/2'>
           <h1>Kevin Shirley</h1>
           <h2>Software Developer</h2>
           <p>I build tools for the web and beyond.</p>
           <ul>
-            <li>LinkedIn</li>
-            <li>Github</li>
-            <li>Email</li>
+            <li>
+              <LinkedInLogoIcon />
+            </li>
+            <li>
+              <GitHubLogoIcon />
+            </li>
+            <li>
+              <EnvelopeClosedIcon />
+            </li>
           </ul>
         </header>
-        <main>
+        <main className='w-1/2'>
           <section>
             <h2>About</h2>
           </section>
@@ -23,9 +31,6 @@ export default function Home() {
             <h2>Projects</h2>
           </section>
         </main>
-        <footer>
-          <small>© 2024 kevinshirley.com</small>
-        </footer>
       </div>
     </div>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'softelo-blue': '#275C85'
+      },
+      spacing: {
+        '20p': '20%'
+      },
     },
   },
   plugins: [],
