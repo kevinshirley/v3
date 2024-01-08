707aacb54277f3b86e50458da9ef2e3043a7ba7c
Kevin Shirley <kevinxshirley@gmail.com>
2024-01-07 19:22:06 -0500

    Header styling layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,66 +4,93 @@ export default function Home() {
   return (
     <div className='bg-slate-900 lg:py-0 lg:px-24 min-h-screen'>
       <div className='flex flex-col lg:flex-row max-w-screen-xl mx-auto'>
-        <header className='w-1/2 lg:sticky top-0 lg:max-h-screen'>
+        <header className='w-1/2 lg:sticky top-0 lg:max-h-screen lg:py-24 flex justify-between flex-col'>
           <div>
-            <h1 className='text-5xl'>Kevin Shirley</h1>
-            <h2>Software Developer</h2>
-            <p>I build tools for the web and beyond.</p>
-            <ul className='lg:hidden'>
-              <li>
-                <LinkedInLogoIcon />
-              </li>
-              <li>
-                <GitHubLogoIcon />
-              </li>
-              <li>
-                <EnvelopeClosedIcon />
-              </li>
-            </ul>
-            <nav className=''>
-              <ul>
-                <li>
-                  <span className="nav-indicator"></span>
-                  <span>About</span>
+            <div>
+              <h1 className='text-5xl text-slate-200 font-bold'>Kevin Shirley</h1>
+              <h2 className='text-slate-200 font-medium mt-3 sm:text-xl'>Software Developer</h2>
+              <p className='text-slate-400 mt-4 max-w-xs'>Building intuitive web solutions, coding the future of user-centric experiences for a connected world.</p>
+              <ul className='lg:hidden'>
+                <li className='text-slate-200'>
+                  <LinkedInLogoIcon />
                 </li>
-                <li>
-                  <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-                  <span>Experience</span>
+                <li className='text-slate-200'>
+                  <GitHubLogoIcon />
                 </li>
-                <li>
-                  <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-                  <span>Projects</span>
+                <li className='text-slate-200'>
+                  <EnvelopeClosedIcon />
+                </li>
+              </ul>
+            </div>
+            <nav className='mt-10 ml-1'>
+              <ul className='w-max'>
+                <li className='list-margin-start-removed nav-item-hover'>
+                  <a href='/' className='flex items-center py-3'>
+                    <span className='nav-indicator nav-indicator-selected mr-4'></span>
+                    <span className='nav-text text-xs font-bold uppercase tracking-widest text-slate-500'>About</span>
+                  </a>
+                </li>
+                <li className='list-margin-start-removed nav-item-hover'>
+                  <a href='/' className='flex items-center py-3'>
+                    <span className='nav-indicator nav-indicator-selected mr-4'></span>
+                    <span className='nav-text text-xs font-bold uppercase tracking-widest text-slate-500'>Experience</span>
+                  </a>
+                </li>
+                <li className='list-margin-start-removed nav-item-hover'>
+                  <a href='/' className='flex items-center py-3'>
+                    <span className='nav-indicator nav-indicator-selected mr-4'></span>
+                    <span className='nav-text text-xs font-bold uppercase tracking-widest text-slate-500'>Projects</span>
+                  </a>
                 </li>
               </ul>
             </nav>
           </div>
+          <div>
+            <ul className='flex justify-start gap-x-6'>
+              <li className='text-slate-200'>
+                <LinkedInLogoIcon className='w-6 h-6 text-slate-400' />
+              </li>
+              <li className='text-slate-200'>
+                <GitHubLogoIcon className='w-6 h-6 text-slate-400' />
+              </li>
+              <li className='text-slate-200'>
+                <EnvelopeClosedIcon className='w-6 h-6 text-slate-400' />
+              </li>
+            </ul>
+          </div>
         </header>
-        <main className='w-1/2'>
+        <main className='w-1/2 lg:pt-24'>
           <section>
-            <h2>About</h2>
+            <div className='hidden'>
+              <h2 className='text-slate-200'>About</h2>
+            </div>
           </section>
           <section>
-            <h2>Experience</h2>
+            <div className='hidden'>
+              <h2 className='text-slate-400'>Experience</h2>
+            </div>
           </section>
           <section>
-            <h2>Projects</h2>
+            <div className='hidden'>
+              <h2 className='text-slate-400'>Projects</h2>
+            </div>
           </section>
-          <section className='mt-10 mb-10'>
+          <section className='mt-10 mb-10 text-slate-400'>
             What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </section>
-          <section className='mt-10 mb-10'>
+          <section className='mt-10 mb-10 text-slate-400'>
             What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </section>
-          <section className='mt-10 mb-10'>
+          <section className='mt-10 mb-10 text-slate-400'>
             What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </section>
-          <section className='mt-10 mb-10'>
+          <section className='mt-10 mb-10 text-slate-400'>
             What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </section>
-          <section className='mt-10 mb-10'>
+          <section className='mt-10 mb-10 text-slate-400'>
             What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </section>
-          <section className='mt-10 mb-10'>
+          <section className='mt-10 mb-10 text-slate-400'>
             What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </section>
         </main>
