58470629108307cbda8676a0990a9d2f816748c3
kevinshirley <kevinxshirley@gmail.com>
2025-02-07 04:15:24 -0500

    Small about refactor

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -6,7 +6,7 @@ const About: React.FC = () => (
       <h2 className='text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only'>About</h2>
     </div>
     <div>
-      <p className='mb-4 text-slate-400'>My coding journey began in 2015, sparked by curiosity about SEO and HTML. Since then, I’ve built enterprise solutions across industries like finance, insurance and e-commerce.</p>
+      <p className='mb-4 text-slate-400'>My coding journey began in 2015, sparked by curiosity about SEO and HTML. Since then, I’ve built cloud-based enterprise solutions across industries like finance, insurance and e-commerce.</p>
       <p className='mb-4 text-slate-400'>Today, I specialize in React, TypeScript, Node.js, and AWS, crafting scalable full-stack applications. I’m always exploring new technologies to refine my skills and stay ahead in the ever-evolving tech landscape.</p>
       <p className='mb-4 text-slate-400'>Beyond coding, I stay active at the gym, enjoy time with family, and plan my next travel adventure.</p>
     </div>
